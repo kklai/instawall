907794f11adc130cbe979038cef19aa486f2f5cb
kklai <kkrebeccalai@gmail.com>
2014-08-02 14:44:03 -0400

    wrap js in IFFE

--- js/authenticate.js
+++ js/authenticate.js
@@ -1,12 +1,14 @@
-var current_url;
-function authenticate() {
-	current_url = document.URL;
-	console.log(current_url);
-	window.location = "https://instagram.com/oauth/authorize/?client_id=0aebae4494a44f47a29f33de378a31e3&redirect_uri=http://localhost:8000/success&response_type=token";
-}
+(function($){
+	var current_url;
+	function authenticate() {
+		current_url = document.URL;
+		console.log(current_url);
+		window.location = "https://instagram.com/oauth/authorize/?client_id=0aebae4494a44f47a29f33de378a31e3&redirect_uri=http://localhost:8000/success&response_type=token";
+	}
 
-$(document).ready(function(){
-	$('.login').click(function(){
-		authenticate();
-	})
-})
+	$(document).ready(function(){
+		$('.login').click(function(){
+			authenticate();
+		});
+	});
+})(jQuery);
